0eec9e347722a69390e445f2773bf6881499b4c3
ohyeong <oueong@naver.com>
2024-11-04 15:32:49 +0900

    Add prime calculation

--- lib.js
+++ lib.js
@@ -4,7 +4,11 @@ function avg(numbers) {
 }
 
 function prime(num) {   // 소수 판별
-    return false;
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
 }
 
 function factorial(num) {
