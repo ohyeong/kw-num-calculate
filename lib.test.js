8d20681147c9670419572c5eb77b58f0f4644554
ohyeong <oueong@naver.com>
2024-11-04 15:57:15 +0900

    Add prime test

--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,7 @@ test("avg[3, 5, 7] should be 5", () => {
 test("avg[-5, 5] should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime[13] should be true", () => {
+    expect(lib.prime([13])).toBe(true);
+});
